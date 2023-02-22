0bd0a52de6a7382528cd10c7fb1428ca52b34564
Rlss91 <rlss91@gmail.com>
2023-02-22 11:55:46 +0200

    Fragment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
+import { Fragment } from "react";
 import CardComponent from "../components/CardComponent";
 
 const HomePage = () => {
   return (
-    <div>
+    <Fragment>
       <h1>Title</h1>
       <CardComponent
         imgUrl="https://images.freeimages.com/images/large-previews/a3e/wild-horse-1334844.jpg"
         title="Horse"
         description="oiwj34et90843jt0934jtwoie4jtwpeokitmerpokitmewrpokltmslkpermgpklermglk"
       />
-    </div>
+    </Fragment>
   );
 };
 
