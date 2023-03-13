750467e29dc3766360f91c7969df21002efe4567
Rlss91 <rlss91@gmail.com>
2023-03-13 15:48:15 +0200

    redux payload

--- src/store/reduxMarge.js
+++ src/store/reduxMarge.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./counter";
 
+//initial the global redux "state"
 const store = configureStore({
   reducer: {
     counterStore: counterReducer,
